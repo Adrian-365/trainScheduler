5b5baaa7abedfe40b7b7147d1ebd944573059c09
Adrian Nalwalker <analwalker@planetfg.local>
2017-10-29 16:19:58 -0700

    masssive failure

--- logic.js
+++ logic.js
@@ -23,6 +23,7 @@ $("#submit-button").on("click", function(event){
 	name = $("#name").val().trim();
 	destination = $("#destination").val().trim();
 	firstTrainTime = $("#firstTrainTime").val();
+	
 	frequency = $("#frequency").val().trim();
 	
 // push table content to firebase //
@@ -34,20 +35,43 @@ $("#submit-button").on("click", function(event){
 })
 });
 
-// database.ref().orderByChild("dateAdded").limitToLast(10).on("child_added", function(snapshot){
+database.ref().orderByChild("dateAdded").limitToLast(10).on("child_added", function(snapshot){
 
-// // Variables for the firebase data //
-// 	var firebaseName = snapshot.val().name;
-// 	var firebaseRole = snapshot.val().role;
-// 	var firebaseStartDate = snapshot.val().startDate;
-// 	var firebaseMonthlyRate = snapshot.val().monthlyRate;
+// Variables for the firebase data //
+	var firebaseName = snapshot.val().name;
+	var firebaseDestination = snapshot.val().destination;
+	var firebaseFrequency = snapshot.val().frequency;
+	var firebaseFirstTrainTime = snapshot.val().moment(currentTime).format("hh:mm");
 
-// // Variables for calculations //
-// 	var monthsWorked = "NOT SURE"
-// 	var totalBilled = "NOT SURE"
+///-------------------
 
-// // Variables for row to be appended to //
-// 	var tableRow = "<tr><td>" + firebaseName + "</td><td>" + firebaseRole + "</td><td>" + firebaseStartDate + "</td><td>" + "NOT SURE" + "</td><td>" + firebaseMonthlyRate + "</td><td>" + "NOT SURE" + "</td></tr>"
-// 	$("#table tbody").append(tableRow);
+    // var firstTimeConverted = moment(firebaseFirstTrainTime, "hh:mm").subtract(1, "years");
+    // console.log("FIRST TIME CONVERTED:" +firstTimeConverted);
+    console.log("FIRST TRAIN: " +firebaseFirstTrainTime);
+    console.log("frequency: "+ firebaseFrequency);
+    // Current Time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    // Difference between the times
+    var diffTime = moment().diff(moment(firebaseFirstTrainTime), "minutes");
+    console.log("DIFFERENCE IN TIME: " + diffTime);
+    // Time apart (remainder)
+    var tRemainder = diffTime % firebaseFrequency;
+    console.log(tRemainder);
+    // Minute Until Train
+    var tMinutesTillTrain = firebaseFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"))
 
-// });
+
+
+
+///--------------------------
+
+// Variables for row to be appended to //
+	var tableRow = "<tr><td>" + firebaseName + "</td><td>" + firebaseDestination + "</td><td>" + firebaseFrequency + "</td><td>" + nextTrain + "</td><td>" + tMinutesTillTrain;
+	$("#table tbody").append(tableRow);
+
+});
